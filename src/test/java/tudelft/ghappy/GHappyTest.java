14da4f034417b0f0f1902e077fca7ff8c6e1a955
Kostas Leivadaris <k.livadaris@gmail.com>
2019-12-02 01:56:48 +0200

    Start implementation of GHappyTest

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,15 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class GHappyTest {
+
+    private GHappy gHappy = new GHappy();
+
+    @Test
+    public void happyStartsWithG() {
+        boolean result = gHappy.gHappy("ggarwva");
+        Assertions.assertEquals(true, result);
+    }
 }
